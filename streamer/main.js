10d094af882f9d6b8c999fa6fca1cc71bc783238
programmer10110 <programmer10110@yandex.ru>
2020-07-07 17:21:09 +0300

    events

--- streamer/main.js
+++ streamer/main.js
@@ -1,15 +1,23 @@
 // Import
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { ApiPromise, WsProvider} = require('@polkadot/api');
 const { Kafka } = require('kafkajs');
 const config = require('./config.json');
+const util = require('util')
+
+const { GenericEvent } = require('@polkadot/types');
+
+
+function hex_to_string(metadata) {
+    return metadata.match(/.{1,2}/g).map(function(v){
+      return String.fromCharCode(parseInt(v, 16));
+    }).join('');
+  }
 
 
 // const kafka = new Kafka({
 //     clientId: 'polkadot-streamer',
 //     brokers: [config.kafka_uri]
 //   })
-  
-
 
 async function main () {
     // Construct
@@ -22,7 +30,35 @@ async function main () {
         if (!blockHash) {
             blockHash = await api.rpc.chain.getBlockHash(height);
         }
+
+        block_events = [];
+
         const events = await api.query.system.events.at(blockHash);
+        events.forEach((record) => {
+            const { event, phase } = record;
+            const types = event.typeDef;
+
+            block_event = {
+                "section": event.section,
+                "method": event.method,
+                "phase": phase.toJSON(),
+                "meta": event.meta.toJSON(),
+            }
+
+            event_data = [];
+            event.data.forEach((data, index) => {
+                event_data.push(
+                    {
+                        "type": types[index].type,
+                        "data": data.toString()
+                    }
+                    );
+              });
+            
+            block_event["data"] = event_data;
+            block_events.push(block_event);
+          });
+
         const signedBlock = await api.rpc.chain.getBlock(blockHash);
         extrinsics = []
         signedBlock.block.extrinsics.forEach((ex, index) => {
@@ -40,9 +76,9 @@ async function main () {
                 }
             },
             'extrinsics' : [...extrinsics],
-            'events': events.toJSON()
+            'events': block_events
         }
-        console.log(block_data.block.header.number, block_data.block.header.hash)
+        console.log(util.inspect(block_data, false, null, true));
         // await producer.send({
         //     topic: 'event',
         //     messages: [
