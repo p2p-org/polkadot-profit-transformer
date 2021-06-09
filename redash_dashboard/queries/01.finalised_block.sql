5382869258e46761ef48f0c5d5f98c55c4ab8f2c
Maxim Manylov <manylov@gmail.com>
2021-06-09 18:32:50 +0200

    add dashboard scripts

---
+++ redash_dashboard/queries/01.finalised_block.sql
@@ -0,0 +1,14 @@
+-- Finalised block
+-- Finalised block
+-- {"description": "", "type": "COUNTER", "options": { "tooltipFormat": "0,0.000", "rowNumber": 1, "stringDecChar": ".", "stringDecimal": 0, "counterColName": "id", "counterLabel": "", "stringThouSep": ",", "targetRowNumber": 1 }, "name": "Finalised block", "query_id": 20 }
+-- {"options": { "parameterMappings": {}, "isHidden": false, "position": { "autoHeight": false, "sizeX": 2, "sizeY": 5, "maxSizeY": 1000, "maxSizeX": 6, "minSizeY": 1, "minSizeX": 1, "col": 0, "row": 0 } }, "text":"","width":1,"dashboard_id":1,"visualization_id":54}
+select block_time, b.era, b.session_id, b.id, b.author, count(distinct e.id) extrinsics, count(distinct ev.id) events, b.hash
+from dot_polka.blocks b
+inner join dot_polka.extrinsics e
+on e.block_id = b.id
+inner join dot_polka.events ev
+on  ev.block_id = b.id
+where parent_id is null
+group by b.id, block_time, b.era, author, hash
+order by id desc
+limit 10
---
+++ redash_dashboard/queries/18.Average rewards per validator.sql
@@ -0,0 +1,30 @@
+-- Average rewards per validator
+-- Average rewards per validator
+-- { "description": "", "type": "CHART", "options": { "showDataLabels": false, "direction": { "type": "counterclockwise" }, "missingValuesAsZero": true, "error_y": { "visible": true, "type": "data" }, "numberFormat": "0,0[.]00000", "yAxis": [ { "type": "linear" }, { "type": "linear", "opposite": true } ], "series": { "stacking": null, "error_y": { "visible": true, "type": "data" } }, "globalSeriesType": "line", "percentFormat": "0[.]00%", "sortX": true, "seriesOptions": { "avg_rewards": { "index": 0, "name": "DOT", "yAxis": 0, "color": "#00B6EB", "zIndex": 0, "type": "line" } }, "valuesOptions": {}, "xAxis": { "labels": { "enabled": true }, "type": "-" }, "dateTimeFormat": "DD/MM/YY HH:mm", "columnMapping": { "avg_points": "unused", "avg_rewards": "y", "era": "x", "validators_active": "unused" }, "textFormat": "", "customCode": "// Available variables are x, ys, element, and Plotly\n// Type console.log(x, ys); for more info about x and ys\n// To plot your graph call Plotly.plot(element, ...)\n// Plotly examples and docs: https://plot.ly/javascript/", "legend": { "enabled": false } }, "name": "Average rewards per validator", "query_id": 20 }
+-- { "options": { "parameterMappings": {}, "isHidden": false, "position": { "autoHeight": false, "sizeX": 3, "sizeY": 8, "maxSizeY": 1000, "maxSizeX": 6, "minSizeY": 5, "minSizeX": 1, "col": 3, "row": 28 } }, "text":"","width":1,"dashboard_id":1,"visualization_id":54}
+WITH data as (
+Select  e.era,
+        validators_active,
+        nominator_active,
+        total_reward/10^10 as total_reward, 
+        total_stake/10^10 as total_stake, 
+        total_reward_points,
+        total_reward_points/validators_active as avg_points, 
+        (total_reward/validators_active)/10^10 as avg_rewards
+FROM dot_polka.eras as e
+    INNER JOIN (SELECT era, COUNT(account_id) as validators_active FROM dot_polka.validators GROUP BY era) as vmax 
+            ON vmax.era = e.era
+    INNER JOIN (SELECT era, COUNT(account_id) as nominator_active FROM dot_polka.nominators GROUP BY era) as nmax 
+            ON nmax.era = e.era)
+SELECT  era,
+        validators_active,
+        nominator_active,
+        total_reward,
+        total_stake,
+        total_reward_points,
+        nominator_active/validators_active as nom_per_val,
+        avg_points,
+        avg_rewards
+FROM data
+order by era desc
+
