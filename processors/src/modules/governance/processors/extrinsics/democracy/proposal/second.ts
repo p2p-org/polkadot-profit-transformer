abe42a063c0deb38ac99df50aa2071c2763056be
Maxim Manylov <manylov@gmail.com>
2021-09-27 11:16:17 +0300

    refactor democracy.second extrincis

--- processors/src/modules/governance/processors/extrinsics/democracy/proposal/second.ts
+++ processors/src/modules/governance/processors/extrinsics/democracy/proposal/second.ts
@@ -2,43 +2,28 @@ import { DemocracyProposalModel } from '../../../../../../apps/common/infra/post
 import { GovernanceRepository } from '../../../../../../apps/common/infra/postgresql/governance/governance.repository'
 import { Logger } from 'apps/common/infra/logger/logger'
 import { ExtrincicProcessorInput } from '../..'
+import { Compact, u32 } from '@polkadot/types'
 
 export const processDemocracyProposalSecondExtrinsic = async (
   args: ExtrincicProcessorInput,
   governanceRepository: GovernanceRepository,
   logger: Logger,
 ): Promise<void> => {
-  const { extrinsicEvents, fullExtrinsic, extrinsic } = args
+  const { fullExtrinsic, extrinsic } = args
 
-  console.log('PROPOSAL SECOND')
-  console.log('EXTRINSIC', JSON.stringify(extrinsic, null, 2))
+  logger.info('democracy.second extrinsic', extrinsic.id)
 
-  // find balance reserved for this seconding
-
-  const balanceReserverdEvent = extrinsicEvents.find((event) => {
-    return event.event.section === 'balances' && event.event.method === 'Reserved'
-  })
-
-  if (!balanceReserverdEvent) {
-    logger.warn('no balance.reserved event for extrinsic ' + extrinsic.id)
-    return
-  }
-
-  console.log('balanceReserverdEvent', balanceReserverdEvent.toHuman())
-
-  const proposalId = extrinsic.args[0] as number
-  const seconds_upper_bound = (extrinsic.args[1] as number) ?? null
-
-  const balanceReserved = parseInt(balanceReserverdEvent.event.data[1].toHex(), 16)
+  const proposalId = <Compact<u32>>fullExtrinsic.args[0]
+  const seconds_upper_bound = <Compact<u32>>fullExtrinsic.args[1]
 
   const proposal: DemocracyProposalModel = {
-    id: proposalId,
+    id: proposalId.toNumber(),
     hash: '',
     block_id: extrinsic.block_id,
     event_id: '',
     extrinsic_id: extrinsic.id,
     event: 'Second',
-    data: { signer: extrinsic.signer, seconds_upper_bound, balanceReserved },
+    data: { signer: extrinsic.signer, seconds_upper_bound },
   }
 
   await governanceRepository.democracy.proposal.save(proposal)
