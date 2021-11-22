c4cb013b6180ed403303e7c89c4c18d93d5bac29
Maxim Manylov <manylov@gmail.com>
2021-11-22 12:21:51 +0300

    change moonbeam api verision

--- main/src/apps/common/infra/polkadotapi/index.ts
+++ main/src/apps/common/infra/polkadotapi/index.ts
@@ -1,10 +1,10 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
-import { typesBundle } from 'moonbeam-types-bundle'
+import { typesBundlePre900 } from 'moonbeam-types-bundle'
 
 export const polkadotFactory = async (nodeUrl: string): Promise<ApiPromise> => {
   const provider = new WsProvider(nodeUrl)
   //@ts-ignore
-  const api = await ApiPromise.create({ provider, typesBundle })
+  const api = await ApiPromise.create({ provider, typesBundlePre900 })
 
   return api
 }
