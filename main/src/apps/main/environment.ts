70a86655c78464f0afbbf344b29dc65c65fe634c
Maxim Manylov <manylov@gmail.com>
2022-04-05 18:59:07 +0700

    add basic_auth to env

--- main/src/apps/main/environment.ts
+++ main/src/apps/main/environment.ts
@@ -9,6 +9,7 @@ export type Environment = {
   LOG_LEVEL: string | undefined
   SUBSTRATE_URI: string | undefined
   REST_API_PORT: number
+  BASIC_AUTH: boolean | undefined
   REST_API_BASIC_AUTH_PASSWORD: string
   PRELOAD: boolean
   START_BLOCK_ID: number
@@ -21,6 +22,7 @@ export const environment: Environment = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   SUBSTRATE_URI: process.env.SUBSTRATE_URI,
   REST_API_PORT: Number(process.env.REST_API_PORT) || 3000,
+  BASIC_AUTH: process.env.BASIC_AUTH ? process.env.BASIC_AUTH === 'true' : false,
   REST_API_BASIC_AUTH_PASSWORD: process.env.REST_API_BASIC_AUTH_PASSWORD ?? 'password',
   PRELOAD: process.env.PRELOAD ? process.env.PRELOAD === 'true' : false,
   START_BLOCK_ID: process.env.START_BLOCK_ID ? Number(process.env.START_BLOCK_ID) : -1, // -1 = continue from last preloaded block from db
