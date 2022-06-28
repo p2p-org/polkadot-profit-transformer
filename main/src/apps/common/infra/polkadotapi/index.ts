622c701d8d036555b9b147d54e61f4ea3fc8c7c1
Maxim Manylov <manylov@gmail.com>
2022-06-28 20:33:18 +0700

    fix deps

--- main/src/apps/common/infra/polkadotapi/index.ts
+++ main/src/apps/common/infra/polkadotapi/index.ts
@@ -1,7 +1,7 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import { typesBundlePre900 } from 'moonbeam-types-bundle'
-// import '@moonbeam-network/api-augment'
-// import '@moonbeam-network/api-augment/moonriver'
+import '@moonbeam-network/api-augment'
+import '@moonbeam-network/api-augment/moonriver'
 
 export const polkadotFactory = async (nodeUrl: string): Promise<ApiPromise> => {
   const provider = new WsProvider(nodeUrl)
