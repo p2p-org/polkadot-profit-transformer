592095ad22333fb0672c7fc370adafaf685cef53
Maxim Manylov <manylov@gmail.com>
2022-06-30 21:15:14 +0700

    rabbit repo

--- main/src/apps/common/infra/rabbitmq/index.ts
+++ main/src/apps/common/infra/rabbitmq/index.ts
@@ -1,9 +1,10 @@
 import { Channel, Connection, ConsumeMessage } from 'amqplib'
+import { environment } from '@apps/main/environment'
+import { logger } from '../logger/logger'
 
 export enum QUEUES {
-  Staking = 'Staking',
-  Governance = 'Governance',
-  Identity = 'Identity',
+  Blocks = 'process_blocks',
+  Staking = 'process_staking',
 }
 
 export type QueueProcessor = {
@@ -17,12 +18,13 @@ export type Rabbit = {
 
 export const RABBIT = async (connection: Connection): Promise<Rabbit> => {
   const channel: Channel = await connection.createChannel()
-  await channel.assertQueue(process.env.NETWORK + ':' + QUEUES.Staking)
+  // await channel.assertQueue(environment.NETWORK + ':' + QUEUES.Staking)
+  await channel.assertQueue(environment.NETWORK + ':' + QUEUES.Blocks)
   await channel.prefetch(1)
 
   return {
     send: async (queue: QUEUES, message: any) => {
-      await channel.sendToQueue(process.env.NETWORK + ':' + queue, Buffer.from(JSON.stringify(message)))
+      await channel.sendToQueue(environment.NETWORK + ':' + queue, Buffer.from(JSON.stringify(message)))
     },
     process: async (queue: QUEUES, processor: QueueProcessor) => {
       const consumer =
@@ -30,7 +32,7 @@ export const RABBIT = async (connection: Connection): Promise<Rabbit> => {
         async (msg: ConsumeMessage | null): Promise<void> => {
           if (msg) {
             // Display the received message
-            console.log('rabbit received message', msg.content.toString())
+            logger.debug('rabbit received message', msg.content.toString())
             const jsonMessage = JSON.parse(msg.content.toString())
             await processor.process(jsonMessage)
             // Acknowledge the message
@@ -38,7 +40,7 @@ export const RABBIT = async (connection: Connection): Promise<Rabbit> => {
             channel.ack(msg)
           }
         }
-      await channel.consume(process.env.NETWORK + ':' + queue, consumer(channel))
+      await channel.consume(environment.NETWORK + ':' + queue, consumer(channel))
     },
   }
 }
