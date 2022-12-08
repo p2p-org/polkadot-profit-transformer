d79f431f19fc48158345b1b6099f659fbd841fef
Ivan Sidorov <yvan.sidorov@gmail.com>
2022-12-09 01:26:50 +0800

    Identity processor sql & docker container spec

--- db/000001_init.sql
+++ db/000001_init.sql
@@ -167,14 +167,30 @@ CREATE TABLE networks (
     "network_id" INT,
     "name" VARCHAR (50),
     "decimals" INT
-
 )
 
+CREATE TABLE identity (
+    "network_id" INT,
+    "account_id" varchar(50),
+    "parent_account_id" varchar(50),
+    "display" varchar(256),
+    "legal" varchar(256),
+    "web" varchar(256),
+    "riot" varchar(256),
+    "email" varchar(256),
+    "twitter" varchar(256),
+    "judgement_status" varchar(256),
+    "registrar_index" BIGINT,
+    "created_at_block_id" BIGINT,
+    "killed_at_block_id" BIGINT,
+    "row_id" SERIAL,
+    PRIMARY KEY ("row_id"),
+    UNIQUE ("account_id", "network_id")
+);
+CREATE INDEX identity_parent_idx ON public.identity ("parent_account_id", "network_id");
 
-alter table blocks add column metadata JSONB;
-alter table rounds add column runtime INT;
+
 create index events_block_id_idx on events (block_id);
-
 create index rounds_round_id_idx on rounds (round_id);
 create index collators_round_id_idx on collators (round_id);
 create index deleagators_round_id_idx on delegators (round_id);
@@ -187,4 +203,7 @@ create index nominators_era_id_idx on nominators (era_id);
 CREATE INDEX blocks_block_id_idx ON public.blocks (block_id);
 
 CREATE INDEX extrinsics_signer_idx ON public.extrinsics (signer);
+--new. need to produce evrywhere
+CREATE INDEX extrinsics_section_idx ON public.extrinsics ("section","method");
+CREATE INDEX events_section_idx ON public.events ("section","method");
 
