549729e8c463e53b7ad8bf3f945d95395d8baef5
I <ivan.sidorov@p2p.org>
2024-10-24 17:40:01 +0800

    Update index.ts

--- main/src/modules/index.ts
+++ main/src/modules/index.ts
@@ -34,7 +34,7 @@ export const ModulesLoader = async (): Promise<void> => {
       environment.NETWORK === 'avail'
     ) {
       PolkadotStakingProcessor()
-      if (environment.NETWORK === 'polkadot' || environment.NETWORK === 'kusama' | environment.NETWORK === 'avail') {
+      if (environment.NETWORK === 'polkadot' || environment.NETWORK === 'kusama' || environment.NETWORK === 'avail') {
         NominationPoolsProcessor()
       }
     } else if (environment.NETWORK === 'moonbeam' || environment.NETWORK === 'moonriver' || environment.NETWORK === 'manta') {
